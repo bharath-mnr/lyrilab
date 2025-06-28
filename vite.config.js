5c5f8c0cd2e0c3eee3bdf6a02f05585e1dda4c4d
bharath-mnr <bharathmnr@outlook.com>
2025-06-28 22:42:55 +0530

    Fix: use loadEnv for VITE_BASE_PATH in Vite config to resolve process.env issue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [ tailwindcss(),react()],
+//   base: process.env.VITE_BASE_PATH || '/',
+// })
+
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [ tailwindcss(),react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '') // loads all `.env` values
+
+  return {
+    plugins: [tailwindcss(), react()],
+    base: env.VITE_BASE_PATH || '/',
+  }
 })
